5a3ba89b4fb57143ddf0ffaf084b6d3d72e961c1
git-leon <xleonhunter@gmail.com>
2020-02-05 11:19:16 -0500

    'update'

--- 2020Feb05-testautomation/src/main/java/com/github/curriculeon/MyObject.java
+++ 2020Feb05-testautomation/src/main/java/com/github/curriculeon/MyObject.java
@@ -1,24 +1,33 @@
 package com.github.curriculeon;
 
+import com.git_leon.leonium.browsertools.With;
 import com.git_leon.leonium.browsertools.browserhandler.BrowserHandler;
-import com.git_leon.leonium.browsertools.browserhandler.ExpectedBrowserCondition;
+import com.git_leon.leonium.browsertools.browserhandler.WebEntity;
 import com.git_leon.leonium.browsertools.factories.BrowserHandlerFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
+// https://www.guru99.com/first-webdriver-script.html
 public class MyObject implements Runnable {
+    BrowserHandler browser = BrowserHandlerFactory.FIREFOX.getBrowserHandler();
+
     public void run() {
-        BrowserHandler browser = BrowserHandlerFactory.FIREFOX.getBrowserHandler();
         String baseUrl = "http://automationpractice.com/index.php";
         browser.navigateTo(baseUrl);
+        navigateToShirtSearch();
+        selectShirtToBuy();
+    }
 
-        String searchQueryTopId = "search_query_top";
-        WebElement we = browser.getElement(By.id(searchQueryTopId));
-        we.sendKeys("shirt\n");
-        we.submit();
-        ExpectedBrowserCondition.pageState("ready");
-        WebElement firstItemInList = browser.getElement(By.xpath("/html/body/div/div[2]/div/div[3]/div[2]/ul/li/div/div[1]/div/a[1]/img"));
-        firstItemInList.click();
-        ExpectedBrowserCondition.pageState("ready");
+    private void selectShirtToBuy() {
+        By byFadedShortSleeveTShirts = With.attributeValue("alt", "Faded Short Sleeve T-shirts");
+        WebEntity firstElementInList = browser.getWebEntity(byFadedShortSleeveTShirts);
+        firstElementInList.click();
+    }
+
+    private void navigateToShirtSearch() {
+        WebEntity inputSearch = browser.getWebEntity(By.id("search_query_top"));
+        WebEntity buttonSearch = browser.getWebEntity(By.xpath("//*[@id=\"searchbox\"]/button"));
+        inputSearch.sendKeys("shirt");
+        buttonSearch.click();
     }
 }
