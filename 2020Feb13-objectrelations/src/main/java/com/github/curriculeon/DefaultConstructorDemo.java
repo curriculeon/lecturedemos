83f4d161799802d251c5d9c222c0138ea009ff86
git-leon <xleonhunter@gmail.com>
2020-02-24 11:30:44 -0500

    Created  and  directory

---
+++ 2020Feb13-objectrelations/src/main/java/com/github/curriculeon/DefaultConstructorDemo.java
@@ -0,0 +1,8 @@
+package com.github.curriculeon;
+
+/**
+ * Created by leon on 2/13/2020.
+ */
+public class Person { // class signature
+    private String name; // instance variable
+}
---
+++ 2020Feb13-objectrelations/src/main/java/com/github/curriculeon/NullaryConstructorDemo.java
@@ -0,0 +1,7 @@
+package com.github.curriculeon;
+
+/**
+ * Created by leon on 2/13/2020.
+ */
+public class NullaryConstructorDemo {
+}
